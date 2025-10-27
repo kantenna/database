bc78e5dd9152041880153c888c4682b1c81d9325
김건우 <kantenna11@gmail.com>
2025-10-27 17:33:59 +0900

    Oracle 함수 연습

--- scott.sql
+++ scott.sql
@@ -175,3 +175,491 @@ SELECT * FROM EMP e WHERE e.ENAME NOT LIKE '%AM%';
 -- null 값은 비교시 = or != 사용하지 않음
 SELECT * FROM EMP e WHERE e.COMM IS NULL;
 SELECT * FROM EMP e WHERE e.COMM IS NOT NULL;
+
+
+-- 집합 연산자
+-- 합집합(UNION, UNION ALL), 교집합(INTERSECT), 차집합(MINUS)
+-- 합집합 : 출력하려는 열의 개수와 자료형이 일치해야한다.
+-- UNION : 중복 제거
+-- DRPTNO = 10 UNION DEPTNO = 20
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 10
+UNION
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 20;
+
+-- UNION ALL
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 10
+UNION
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 10;
+
+-- MINUS
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+MINUS
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 10;
+
+-- INTERSECT 
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+INTERSECT 
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.SAL
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 10;
+
+-- 실습
+-- 1. 사원 이름이 S로 끝나는 사원 데이터 조회
+SELECT * FROM EMP e WHERE e.ENAME LIKE '%S';
+-- 2. 30번 부서에 근무하고있는 사원 중에 JOB이 SALESMAN인 사원의 사번, 이름, 직책, 급여, 부서번호 조회
+SELECT e.EMPNO, e.ENAME , e.JOB , e.SAL , e.DEPTNO FROM EMP e WHERE e.DEPTNO = 30 AND e.JOB = 'SALESMAN';
+
+-- 3. 20번, 30번 부서에 근무하고있는 사원 중 급여가 2000 초과인 사원을 다음 두 방식의 SELECT 문을 사용하여
+-- 사번, 이름, 직책, 급여, 부서번로를 출력
+-- 집합연산자를 사용하는 방식
+SELECT e.EMPNO , e.ENAME , e.SAL , e.DEPTNO FROM EMP e WHERE e.SAL > 2000 AND e.DEPTNO = 20
+UNION
+SELECT e.EMPNO , e.ENAME , e.SAL , e.DEPTNO FROM EMP e WHERE e.SAL > 2000 AND e.DEPTNO = 30; 
+-- 집합연산자를 사용하지 않는 방식
+SELECT  e.EMPNO , e.ENAME , e.SAL , e.DEPTNO FROM EMP e WHERE e.SAL > 2000 AND e.DEPTNO IN (20,30);
+
+-- 4. NOT BETWEEN A AND B 연산자를 사용하지 않고 급여열이 2000 이상 3000이하 범위 이외의 값을 가진 데이터 조회
+SELECT * FROM EMP e WHERE e.SAL < 2000 AND e.SAL > 3000;
+
+-- 5. 사원 이름에 E가 포함된 30번 부서의 사원중 급여가 1000~2000 사이가 아닌 사원명, 사번, 급여, 부서번호 조회
+SELECT
+	e.ENAME ,
+	e.EMPNO ,
+	e.SAL ,
+	e.DEPTNO
+FROM
+	EMP e
+WHERE
+	e.ENAME LIKE '%E'
+	AND e.DEPTNO = 30
+	AND e.SAL NOT BETWEEN 1000 AND 2000;
+
+--6. 추가 수당이 없고 상급자가 있고 직책이 MANAGER, CLREK인 사원 중에서 사원이름의 두번째 글자가 L이 아닌
+-- 사원의 정보를 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.COMM IS NULL
+	AND e.MGR IS NOT NULL
+	AND e.JOB IN ('MANAGER', 'CLERK')
+	AND e.ENAME NOT LIKE '_L%';
+
+
+-- 함수
+-- 1. 문자함수
+-- UPPER(문자열) : 대문자 변환
+-- LOWER(문자열) : 소문자 변환
+-- INITCAP(문자열) : 첫글자는 대문자, 나머지 문자는 소문자
+-- LENGTH(문자열) : 문자열 길이
+-- LENGTHB(문자열) : 문자열의 바이트 길이
+-- SUBSTR(문자열데이터, 시작위치, 추출길이) : 문자열 부분추출
+-- INSTR(대상문자열, 위치를 찾으려는 문자, 위치찾기 시작위치, 찾으려는 문자가 몇 번째인지) : 문자열 데이터 안에서 트정 문자 위치 찾기
+-- REPLACE(문자열, 찾는문자, 바꿀문자)
+-- CONCAT(문자열1, 문자열2) : 두 문자열 데이터 합치기
+-- TRIM(삭제옵션(선택), 삭제할 문자(선택), FROM 원본 문자열)
+-- 1) 삭제옵션 : LEADING OR TRAILING OR BOTH
+-- LTRIM(원본 문자열, 삭제할 문자열) : 좌
+-- RTRIM(원본 문자열, 삭제할 문자열) : 우
+
+
+SELECT ENAME, UPPER(ENAME), LOWER(ENAME), INITCAP(ENAME)
+FROM EMP e;
+
+SELECT ENAME, LENGTH(ENAME), LENGTHB(ENAME)
+FROM EMP e;
+
+-- DUAL(SYS 소유의 테이블, 더미 테이블)
+-- 임시연산이나 함수의 결과값 확인 용도!
+-- xe21 (한글 한자당 3Byte 사용)
+SELECT LENGTH('한글'), LENGTHB('한글') FROM DUAL;
+
+-- 사원명 길이가 5 이상인 사원조회
+SELECT * FROM EMP e WHERE LENGTH(e.ENAME) >= 5; 
+-- 직택명이 6자 이상인 사원조회
+SELECT * FROM EMP e WHERE LENGTH(e.JOB) >= 6; 
+
+SELECT
+	e.JOB,
+	SUBSTR(e.JOB, 1, 2),
+	SUBSTR(e.JOB , 3, 2),
+	SUBSTR(e.JOB, 5)
+FROM
+	EMP e;
+
+-- emp 테이블에서 사원명을 세번째 글자부터 끝까지 출력
+SELECT e.ENAME , SUBSTR(e.ENAME, 3) FROM EMP e; 
+
+SELECT
+	e.JOB,
+	SUBSTR(e.JOB, -LENGTH(e.JOB)),
+	SUBSTR(e.JOB, -LENGTH(e.JOB), 2),
+	SUBSTR(e.JOB, -3)
+FROM
+	EMP e;
+
+SELECT INSTR('HELLO, ORACLE', 'L') AS 첫번째, INSTR('HELLO, ORACLE', 'L', 5) AS 두번째, INSTR('HELLO, ORACLE', 'L', 2, 2) AS 세번째
+FROM DUAL;
+
+-- 사원명에 문자S가 포함된 사원 조회
+-- 1) LIKE 2) INSTR
+SELECT * FROM  EMP e WHERE INSTR(e.ENAME,'S') > 0;
+
+-- 010-4526-7858 => 010 4526 7858 OR 01045267858
+SELECT '010-4526-7858' AS BEFORE, REPLACE('010-4526-7858', '-', ' ') AS REPLACE1, REPLACE('010-4526-7858', '-') AS REPLACE2 FROM DUAL;
+
+-- concat() or ||
+-- EMPNO, ENAME 합치기
+SELECT CONCAT(e.EMPNO , e.ENAME), CONCAT(e.EMPNO , CONCAT(':', e.ENAME)), e.EMPNO || ':' || e.ENAME  FROM EMP e; 
+
+-- TRIM()
+SELECT
+	'[' || TRIM(' __Oracle__ ') || ']' AS trim,
+	'[' || TRIM(LEADING FROM ' __Oracle__ ') || ']' AS TRIM_LEADING,
+	'[' || TRIM(TRAILING FROM ' __Oracle__ ') || ']' AS TRIM_TRAILING,
+	'[' || TRIM(BOTH FROM ' __Oracle__ ') || ']' AS TRIM_BOTH
+FROM
+	DUAL;
+
+SELECT
+	'[' || TRIM(' _Oracle_ ') || ']' AS trim,
+	'[' || LTRIM(' _Oracle_ ') || ']' AS LTRIM,
+	'[' || LTRIM('<_Oracle_>','_<') || ']' AS LTRIM2,
+	'[' || RTRIM(' _Oracle_ ') || ']' AS RTRIM,
+	'[' || RTRIM('<_Oracle_>','>_') || ']' AS RTRIM2
+FROM
+	DUAL;
+
+
+-- 숫자함수
+-- ROUND(숫자, 반올림 위치) : 반올림
+-- TRUNC(숫자, 버림 위치) : 버림
+-- CEIL(숫자) : 지정된 숫자보다 큰 정수 중 가장 작은 정수 반환
+-- FLOOR(숫자) : 지정된 숫자보다 작은 정수 중 가장 큰 정수 반환
+-- MOD(숫자, 니눌 숫자) : 지정된 숫자를 나눈 나머지 반환
+
+SELECT
+	ROUND(1234.5678) AS ROUND,
+	ROUND(1234.5678, 0) AS ROUND0,
+	ROUND(1234.5678, 1) AS ROUND1,
+	ROUND(1234.5678, 2) AS ROUND2,
+	ROUND(1234.5678,-1) AS ROUND_MINUS1,
+	ROUND(1234.5678,-2) AS ROUND_MINUS2
+FROM
+	DUAL;
+
+SELECT
+	TRUNC(1234.5678) AS TRUNC,
+	TRUNC(1234.5678, 0) AS TRUNC0,
+	TRUNC(1234.5678, 1) AS TRUNC1,
+	TRUNC(1234.5678, 2) AS TRUNC2,
+	TRUNC(1234.5678,-1) AS TRUNC_MINUS1,
+	TRUNC(1234.5678,-2) AS TRUNC_MINUS2
+FROM
+	DUAL;
+
+SELECT CEIL(3.14), FLOOR(3.14), CEIL(-3.14), FLOOR(-3.14)
+FROM DUAL;
+
+SELECT MOD(15,6), MOD(10,2), MOD(11,2)
+FROM DUAL;
+
+-- 날짜함수
+-- 날짜 데이터 + 숫자 : 이후 날짜 반환
+-- 날짜 데이터 - 숫자 : 이전 날짜 반환
+-- 날짜 데이터 + 날짜 데이터 : 연산불가
+-- 날짜 데이터 _ 날짜 데이터 : 두 날짜 데이터 간의 일수 차이 반환
+-- ADD_MONTHS(날짜데이터, 더할 개월수)
+-- MONTHS_BETWEEN(날짜데이터1, 날짜데이터2)
+-- NEXT_DAY(날짜데이터1, 요일문자)
+-- LAST_DAT(날짜데이터)
+
+-- 오라클에서 시스템 날짜 출력 : SYSDATE
+-- CURRENT_DATE
+SELECT SYSDATE, SYSDATE + 1, SYSDATE -1, CURRENT_DATE, CURRENT_TIMESTAMP
+FROM DUAL;
+
+SELECT SYSDATE, ADD_MONTHS(SYSDATE, 3)
+FROM DUAL;
+
+-- 입사 50주년이 되는 날짜 구하기
+SELECT e.HIREDATE, ADD_MONTHS(e.HIREDATE, 600)
+FROM EMP e ;
+
+-- 입사한지 40년이 넘은 사원을 조회
+SELECT e.HIREDATE
+FROM EMP e WHERE ADD_MONTHS(e.HIREDATE, 480) < SYSDATE;
+
+SELECT
+	e.EMPNO ,
+	e.HIREDATE ,
+	SYSDATE,
+	MONTHS_BETWEEN(e.HIREDATE , SYSDATE) AS MONTH1,
+	MONTHS_BETWEEN(SYSDATE, e.HIREDATE) AS MONTH2,
+	TRUNC(MONTHS_BETWEEN(SYSDATE, e.HIREDATE)) AS MONTH3
+FROM
+	EMP e;
+	
+SELECT SYSDATE, LAST_DAY(SYSDATE), NEXT_DAY(SYSDATE, '월요일')
+FROM DUAL;
+
+
+-- 형변환 함수
+-- TO_CHAR() : 날짜, 숫자 데이터를 문자로 변환
+-- TO_NUMBER() : 문자 데이터를 숫자로 변환
+-- TO_DATE() : 문자 데이터를 날짜 데이터로 변환
+
+SELECT SYSDATE, TO_CHAR(SYSDATE, 'YYYY/MM/DD')
+FROM DUAL;
+
+SELECT
+	SYSDATE,
+	TO_CHAR(SYSDATE, 'MM'),
+	TO_CHAR(SYSDATE, 'MON'),
+	TO_CHAR(SYSDATE, 'MONTH'),
+	TO_CHAR(SYSDATE, 'DD'),
+	TO_CHAR(SYSDATE, 'DY'),
+	TO_CHAR(SYSDATE, 'DAY')
+FROM
+	DUAL;
+
+SELECT
+	SYSDATE,
+	TO_CHAR(SYSDATE, 'HH24:MI:SS'),
+	TO_CHAR(SYSDATE, 'HH12:MI:SS AM'),
+	TO_CHAR(SYSDATE, 'HH:MI:SS P.M.')
+FROM
+	DUAL;
+
+-- 자동형변환
+SELECT e.EMPNO , e.ENAME , e.EMPNO + '500'
+FROM EMP e 
+WHERE e.ENAME = 'SMITH';
+
+--SELECT e.EMPNO , e.ENAME , e.EMPNO + 'ABCD'
+--FROM EMP e 
+--WHERE e.ENAME = 'SMITH';
+
+SELECT e.SAL, TO_CHAR(e.sal, '$999,999'), TO_CHAR(e.sal, 'L999,999')
+FROM EMP e 
+
+SELECT 1300 - '1500', '1300' + 1500
+FROM DUAL;
+SELECT '1300' - '1500', '1300' + 1500
+FROM DUAL;
+
+-- SQL Error [1722] [42000]: ORA-01722: 수치가 부적합합니다
+SELECT '1,300' - '1500', '1300' + 1500
+FROM DUAL;
+
+SELECT TO_NUMBER('1,300','999,999') - TO_NUMBER('1,500','999,999'), '1300' + 1500
+FROM DUAL;
+
+SELECT TO_DATE('20251027','YYYY-MM-DD'), TO_DATE('20251027','YYYY/MM/DD')
+FROM DUAL;
+
+SELECT TO_DATE('2025-10-27') - TO_DATE('2025-09-23')
+FROM DUAL;
+
+-- null 처리 함수
+-- 1. NVL(NULL에 해당하는 열, 반환할 데이터) : NULL인 경우만 반환할 데이터로 돌아옴
+-- 2. NVL2(NULL에 해당하는 열, NULL이 아닐때 반환할 데이터, 반환할 데이터)
+-- NULL + NULL
+-- 숫자 + NULL = NULL
+SELECT EMPNO, ENAME, SAL, COMM, COMM + SAL FROM EMP;
+SELECT EMPNO, ENAME, SAL, COMM, NVL(COMM,0) + SAL FROM EMP;
+SELECT EMPNO, ENAME, SAL, COMM, NVL2(COMM,'O','X'),NVL2(COMM, SAL * 12 + COMM ,SAL*12) FROM EMP;
+
+-- DECODE(), CASE() : 상황에 따라 다른 데이터를 반환
+-- 직책이 MANAGER인 사원은 급여의 10%, SALESMAN인 사원은 급여의 5%, ANALYST인 사원은 그대로, 나머지는 3%만큼 인상된 급여 구하기
+-- DECODE(검사 대상이 될 열 또는 데이터,
+-- 조건1, 조건1과 일치 할 때 반환할 데이터,
+-- 조건2, 조건2과 일치 할 때 반환할 데이터,
+-- [위에 나열한 조건과 일치하지 않는 경우 반환할 결과])
+
+-- CASE : 각 조건에 사용되는 데이터가 서로 상관없어도 됨
+-- 		  동등(=) 외에 다양한 조건 사용가능!
+-- CASE 검사 대상이 될 열 또는 데이터
+-- WHEN 조건1 THEN 조건1과 일치할 때 반환할 결과
+-- WHEN 조건2 THEN 조건2과 일치할 때 반환할 결과
+-- WHEN 조건3 THEN 조건3과 일치할 때 반환할 결과
+-- ELSE [위에 나열한 조건과 일치하지 않는 경우 반환할 결과]
+-- END
+
+
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.JOB ,
+	e.SAL,
+	DECODE(e.JOB,
+	'MANAGER', e.SAL * 1.1,
+	'SALESMAN', e.SAL * 1.05,
+	'ANALYST', e.SAL ,
+	e.SAL * 1.03) AS 급여
+FROM
+	EMP e;
+
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	e.JOB ,
+	e.SAL,
+	CASE e.JOB
+		WHEN 'MANAGER' THEN e.SAL * 1.1
+		WHEN 'SALESMAN' THEN e.SAL * 1.05
+		WHEN 'ANALYST' THEN e.SAL
+		ELSE e.SAL * 1.03
+	END AS 급여
+FROM
+	EMP e;
+
+-- COMM이 NULL인 경우에는 해당없음, 0인 경우에는 수당없음, 0보다 큰 경우에는 현재 수당 : 800
+SELECT
+	e.EMPNO,
+	e.ENAME,
+	e.COMM,
+	CASE
+		WHEN e.COMM IS NULL THEN '해당없음'
+		WHEN e.COMM = 0 THEN '수당없음'
+		WHEN e.COMM > 0 THEN '수당 : ' || e.COMM
+	END AS COMM_TEXT
+FROM
+	EMP e;	
+
+-- EMP 테이블에서 사원의 월 평균 근무일수는 21.5일이다.
+-- 하루 근무시간을 8시간으로 보았을 때 사원의 하루 급여(DAY_PAY) 시급(TIME_PAY)를 계산하여 결과를 출력
+-- 하루 급여는 소수 첫째 자리에서 버리고, 시급은 소수 둘째 자리에서 반올림
+SELECT
+	TRUNC(e.SAL / 21.5, 1) AS DAY_PAY ,
+	ROUND(e.SAL / 21.5 / 8 , 2) AS TIME_PAY
+FROM
+	EMP e; 
+
+
+-- EMP 테이블에서 사원은 입사일을 기준으로 3개월이 지난 후 첫 월요일에 정직원이 된다. 사원이 정직원이 되는 날짜(R_JOB)을
+-- YYYY-MM-DD 형식으로 출력. 단, 추가수당이 없는 사원의 추가 수당은 N/A로 출력
+-- EMPNO, ENAME, HIREDATE, R_JOB, COMM 출력
+SELECT
+	e.EMPNO ,
+	e.ENAME ,
+	TO_CHAR(e.HIREDATE, 'YYYY/MM/DD') AS HIRE_DATE,
+	TO_CHAR(NEXT_DAY(ADD_MONTHS(e.HIREDATE, 3), '월요일'), 'YYYY/MM/DD') AS R_JOB,
+	NVL(TO_CHAR(e.COMM), 'N/A') AS COMM
+FROM
+	EMP e ;
+
+
+-- EMP 테이블의 모든 사원을 대상으로 직속 상관의 사원번호(MGR)을 아래의 조건을 기준으로 변환해서 CHG_MGR열에 출력
+-- 조건
+-- 직속 상관의 번호가 없는 경우 0000
+-- 직속 상관의 사원번호 앞 두자리가 75 일때 5555
+-- 직속 상관의 사원번호 앞 두자리가 76 일때 6666
+-- 직속 상관의 사원번호 앞 두자리가 77 일때 7777
+-- 직속 상관의 사원번호 앞 두자리가 78 일때 8888
+-- 그 외 직속 상관 사원 번호일때 : 본래 직속 상관의 사원번호 그대로 출력
+SELECT
+	e.EMPNO ,
+	e.ENAME,
+	e.HIREDATE,
+	CASE SUBSTR(TO_CHAR(NVL(e.MGR, 0)),1,2)
+		WHEN '0' THEN '0000'
+		WHEN '75' THEN '5555'
+		WHEN '76' THEN '6666'
+		WHEN '77' THEN '7777'
+		WHEN '78' THEN '8888'
+		ELSE TO_CHAR(e.MGR)
+	END AS CHG_MGR
+FROM
+	EMP e;
+
+
+-- 다중행 함수
+-- SUM, AVG, COUNT, MAX, MIN
+
+SELECT SUM(e.SAL), AVG(e.SAL), MAX(e.SAL), MIN(e.SAL), COUNT(e.SAL)
+FROM EMP e ;
+
+SELECT SUM(DISTINCT e.SAL), AVG(e.SAL), MAX(e.SAL), MIN(e.SAL), COUNT(*)
+FROM EMP e ;
+
+-- SQL Error [937] [42000]: ORA-00937: 단일 그룹의 그룹 함수가 아닙니다
+--SELECT SUM(e.SAL), e.ENAME
+--FROM EMP e ;
+
+-- 10번 부서의 급여총계, 평균 구하기
+SELECT SUM(e.SAL), AVG(e.SAL)
+FROM EMP e WHERE e.DEPTNO = 10;
+
+-- 20번 부서의 제일 오래된 입사일
+SELECT MIN(e.HIREDATE)
+FROM EMP e WHERE e.DEPTNO = 20;
+
+-- 20번 부서의 제일 최신 입사일
+SELECT MAX(e.HIREDATE)
+FROM EMP e WHERE e.DEPTNO = 20;
+
+
+
+
+-- GROUP BY
+-- 부서별 급여 평균
+-- 다중행 함수 옆에 올 수 있는 칼럼은 GROUP BY에 사용한 컬럼만 가능!!!
+SELECT e.DEPTNO , AVG(e.SAL)
+FROM EMP e 
+GROUP BY e.DEPTNO;
+
+
+
+
