745928a21fedd027987b34cb041c554dee8dba44
김건우 <kantenna11@gmail.com>
2025-10-30 18:17:34 +0900

    restraint, user

--- root.sql
+++ root.sql
@@ -75,6 +75,17 @@ create database if not exists EXAM;
 use EXAM;
 
 -- 테이블 생성
+-- 데이터 유형
+-- 숫자형 : TINYINT(1 Byte), SMALLINT(2 Byte), MEDIUMINT(3 Byte), INT(4 Byte), BIGINT(8 Byte)
+-- 실수형 : 
+-- 1. 고정소수점 방식 : DECIMAL / NUMERIC
+-- 2. 부동소수점 방식 : FLOAT / DOUBLE
+-- 문자형 :
+-- 1. CHAR(n) - 고정 길이 문자형
+-- 2. VARCHAR(n)
+-- 날짜형 : TIME / DATE / DATETIME / TIMESTAMP
+
+
 create table TABLE1(
 	COL1 INT,
 	COL2 VARCHAR(50),
@@ -88,7 +99,74 @@ create table TABLE2(
 );
 
 insert into TABLE2(COL2,COL3) values('TEST','2025-10-29');
+insert into TABLE2(COL1, COL2,COL3) values(3, 'TEST','2025-10-30');
+
+
 select * from TABLE2;
+
+-- 현재 auto_increment 로 생성된 마지막 값 확인
+select LAST_INSERT_ID();
+
+-- auto_increment 시작값 변경
+alter table table2 auto_increment = 100;
+
+-- auto_increment 증가값 변경
+-- set @@AUTO_INCREMENT_INCREMENT = 1;
+
+create table EXAM_INSERT_SELECT_FROM(
+	COL1 INT,
+	COL2 VARCHAR(10)
+);
+
+create table EXAM_INSERT_SELECT_TO(
+	COL1 INT,
+	COL2 VARCHAR(10)
+);
+
+insert into EXAM_INSERT_SELECT_FROM(col1,col2) values (1,'Do');
+insert into EXAM_INSERT_SELECT_FROM(col1,col2) values (2,'It');
+insert into EXAM_INSERT_SELECT_FROM(col1,col2) values (3,'MySQL');
+
+-- EXAM_INSERT_SELECT_FROM => EXAM_INSERT_SELECT_TO
+
+
+insert into EXAM_INSERT_SELECT_TO select * from EXAM_INSERT_SELECT_FROM;
+select * from EXAM_INSERT_SELECT_TO;
+
+create table EXAM_SELECT_NEW as select * from EXAM_INSERT_SELECT_FROM;
+select * from exam.exam_select_new esn ;
+
+
+create table EXAM_DATE_TABLE(
+	COL1 DATE, COL2 TIME, COL3 DATETIME, COL4 TIMESTAMP);
+
+insert into EXAM_DATE_TABLE values (NOW(),NOW(),NOW(),NOW());
+
+select * from exam.exam_date_table edt ;
+
+-- 사용자 생성
+-- 아이디 대소문자 구분함!!!!
+
+-- localhost : 내 컴퓨터( 로컬 접속만 가능)
+-- % : 모든 아이피에서 접속가능(외부접속 가능)
+CREATE USER 'TEST1'@'loacalhost' IDENTIFIED BY '12345';
+-- CREATE USER 'TEST1'@'%' IDENTIFIED BY '12345';
+
+-- 권한 부여
+-- grant 권한 목록 on 데이터베이스명.테이블 to '사용자이름'@'호스트'
+-- grant select, insert,update on exam.table1 to 'test1'@'localhost'
+grant all privileges on exam.* to 'test1'@'localhost';
+-- 변경사항 반영
+flush privileges;
+
+-- 사용자 삭제
+drop user 'TEST1'@'loacalhost';
+
+-- 비밀번호 변경
+-- ALTER USER 'TEST1'@'loacalhost' IDENTIFIED BY 변경할 비밀번호;
+
+
+
 
 
 
--- system.sql
+++ system.sql
@@ -12,3 +12,52 @@ GRANT CREATE VIEW TO SCOTT;
 
 GRANT CREATE SYNONYM TO SCOTT;
 GRANT CREATE PUBLIC SYNONYM TO SCOTT;
+
+
+-- 사용자
+-- 데이터베이스에 접속하여 데이터 관리하는 계정
+
+-- 오라클 데이터베이스
+-- 테이블, 뷰, 인덱스, 시퀀스... => 업무별 사용자 생성 후 객체 생성 할 수있는 권한 부여
+
+-- CREATE USER 사용자이름 IDENTIFIED BY 비밀번호;
+
+-- SQL Error [65096] [99999]: ORA-65096: 공통 사용자 또는 롤 이름이 부적합합니다. => C## 접두어 필요
+CREATE USER TEST1 IDENTIFIED BY 12345; 
+CREATE USER C##TEST1 IDENTIFIED BY 12345; 
+
+CREATE USER TEST2 IDENTIFIED BY 12345; 
+
+-- 사용자 TEST2는 CREATE SESSION 권한을 가지고 있지 않음; 로그온이 거절되었습니다
+GRANT CREATE SESSION TO TEST2;
+
+-- 개별 권한 묶어서 관리 => 롤
+GRANT CONNECT, RESOURCE TO TEST2;
+
+-- 테이블스페이스 'USERS'에 대한 권한이 없습니다.
+-- ALTER USER TEST2 DEFAULT TABLESPACE USERS QUOTA 2M IN USERS;
+
+
+-- 사용자 생성 --------------------------
+-- 아이디 대소문자 구별안함 / 비밀번호만 구별함 --
+ALTER SESSION SET "_oracle_script"=TRUE;
+
+CREATE USER TEST3 IDENTIFIED BY 12345
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP
+QUOTA 10M ON USERS;
+
+GRANT CONNECT, RESOURCE TO TEST3;
+-------------------------------------
+
+--사용자 삭제
+-- SQL Error [1922] [42000]: ORA-01922: 'TEST2'(을)를 삭제하려면 CASCADE를 지정하여야 합니다
+DROP USER test2 CASCADE;
+DROP USER test3 CASCADE;
+
+-- 권한 취소
+--REVOKE 취소할권한이름 FROM 사용자명
+REVOKE CREATE SESSION FROM test2;
+
+-- 비밀번호 변경
+-- ALTER USER test3 IDENTIFIED BY 변경할 비밀번호;
